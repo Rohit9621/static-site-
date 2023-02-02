c9be83615e8e17e64da287e52d8daf33be926d49
ROHIT PRAJAPATI <122344055+Rohit9621@users.noreply.github.com>
2023-02-03 02:16:13 +0530

    Add files via upload

---
+++ Bulk SMS Service.php
@@ -0,0 +1,39 @@
+<?php include_once("include/header.php");?>
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Bulk SMS Service</h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li>Bulk SMS Service</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	<!-- ======= Footer ======= -->
+<?php include_once("include/footer.php");?>
